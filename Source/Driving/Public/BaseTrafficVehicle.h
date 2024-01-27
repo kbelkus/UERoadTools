5bb494a8eb2db51e1a7b175d154ffe69998fb197
Kieran Belkus <kieranbelkus@gmail.com>
2024-01-28 08:53:02 +0900

    Updated Source

--- Source/Driving/Public/BaseTrafficVehicle.h
+++ Source/Driving/Public/BaseTrafficVehicle.h
@@ -19,16 +19,23 @@ public:
 
 	UFUNCTION()
 	void SetSpawnLocation();
-	
+
 	//Vehicle Logic
 	UFUNCTION()
 	void Driving(float dt);
+	UFUNCTION()
+	void VehicleTargetUpdate(float dt);
+	UFUNCTION()
+	FVector ResolveVehicleLocation(FVector SplineLocation, FRotator Rotation);
+	
 
+	//Car Target
 	UFUNCTION()
 	void GetNextLane();
-
 	UFUNCTION()
 	void CollisionCheck();
+	UFUNCTION()
+	void DebugDrawTarget();
 
 
 protected:
@@ -49,6 +56,21 @@ public:
 		return FMath::RandRange(Min, Max);
 	}
 		
+
+	//Car Tweakables
+	UPROPERTY(EditAnywhere)
+		TArray<FVector> WheelOffsets = 
+	{ 
+		FVector(150.0f,75.0f,0.0f),
+		FVector(150.0f,-75.0f,0.0f),
+		FVector(-100.0f,75.0f,0.0f),
+		FVector(-100.0f,-75.0f,0.0f)
+	};
+
+	UPROPERTY(EditAnywhere)
+	TArray<FVector> WheelVectors;
+	UPROPERTY(EditAnywhere)
+	TArray<FVector> WheelRaycastPositions;
 
 	//Car Current Values -- What is the car doing on this frame?
 	UPROPERTY(EditAnywhere)
@@ -71,26 +93,48 @@ public:
 	bool isValidVehicle = false;
 	UPROPERTY(EditAnywhere)
 	int LaneDrivingDirection;
-	
+	UPROPERTY(EditAnywhere)
+	bool IgnoreSignal = false;
+
+	UPROPERTY(EditAnywhere)
+	FString CollidedObject;
+	FVector CollidedObjectLocation;
+
+	//Target Properties
 	int LaneStatus = 1;
+	int NextLaneID;
+	ALaneSpline* NextLane;
+	USplineComponent* NextSpline;
+	float NextLaneLength;
+	float NextPositionAlongSplineLength;
+	int NextLaneDrivingDirection;
+	UPROPERTY(EditAnywhere)
+	int NextLaneStatus = 1;
+
+
 
 	FVector RayHeightOffset = FVector(0, 0, 100);
-
 
 	//Vehicle Target Properties - Intended Direction, Collision Detection etc
 	FVector ForwardVector;
 	FVector RayStartPosition;
+	UPROPERTY(EditAnywhere)
 	bool detectedObstacle = false;
+	UPROPERTY(EditAnywhere)
 	float DistanceToObstacle;
 	FCollisionQueryParams CollisionParms;
 	int CollidedLaneDrivingDirection;
-
+	FVector TargetLocation;
+	UPROPERTY(EditAnywhere)
+	bool FoundNextLane = false; //Check so that, we dont keep getting a new lane when we are sat at traffic light
 
 	//Vehicle Properties - Stuff thatcont
 	UPROPERTY(EditAnywhere)
 	float VehicleMaxSpeed = 60.0f; //Max Speed
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* VehicleMesh;
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* VehicleBodyMesh;
 
 	//Vehicle Init properties -- Set by Traffic Manager
 	UPROPERTY(EditAnywhere)
@@ -98,8 +142,9 @@ public:
 	UPROPERTY(EditAnywhere)
 	int VehicleID = 0;
 
-
-
+	//Vehicle Traffic Manager Properties - Updating Spawning etc
+	UPROPERTY(VisibleAnywhere)
+	bool MarkedForRespawn = false;
 
 
 
--- Source/Driving/Public/TrafficManager.h
+++ Source/Driving/Public/TrafficManager.h
@@ -4,7 +4,71 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "BaseTrafficVehicle.h"
+#include "LaneSpline.h"
+#include "TrafficGridObject.h"
 #include "TrafficManager.generated.h"
+
+
+USTRUCT()
+struct FLanes
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	ALaneSpline* Lane;
+	UPROPERTY(EditAnywhere)
+	FVector LanePosition;
+
+	FLanes()
+	{
+
+	}
+};
+
+
+USTRUCT()
+struct FTrafficAgents
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	ABaseTrafficVehicle* TrafficAgent;
+	UPROPERTY(EditAnywhere)
+	float DistanceFromPlayer;
+	UPROPERTY(EditAnywhere)
+	float PreviousDistanceFromPlayer;
+	UPROPERTY(EditAnywhere)
+	float delta;
+	UPROPERTY(EditAnywhere)
+	int PositiveDeltaCount = 0;
+
+	FTrafficAgents()
+	{
+
+	}
+};
+
+
+USTRUCT()
+struct FLaneSplinePoints
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	ALaneSpline* ReferencedLaneSpline;
+	UPROPERTY(VisibleAnywhere)
+	TArray<FVector> PointLocation;
+	UPROPERTY(VisibleAnywhere)
+	TArray<int> PointID;
+
+	struct FLaneSplinePoints()
+	{
+
+	}
+};
+
+
 
 UCLASS()
 class DRIVING_API ATrafficManager : public AActor
@@ -15,6 +79,31 @@ public:
 	// Sets default values for this actor's properties
 	ATrafficManager();
 
+	UFUNCTION()
+	void GetLanes();
+	UFUNCTION()
+	void InitialiseTrafficAgents();
+	UFUNCTION()
+	void UpdateTrafficAgents();
+	UFUNCTION()
+	void UpdateSingleTrafficAgent(FTrafficAgents TrafficAgent);
+	UFUNCTION()
+	void UpdatePlayerCell();
+	UFUNCTION()
+	void RespawnTrafficAgents();
+
+	//Traffic Grid
+	UFUNCTION()
+	void CreateTrafficGrid();
+	UFUNCTION()
+	void GetAllLaneSplinePoints();
+	UFUNCTION()
+	void AssignLanesToCells();
+	UFUNCTION()
+	void AssignNeighbourCells();
+	UFUNCTION(CallInEditor)
+	void DestroyNeighbourCells();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,8 +111,70 @@ protected:
 	//Override Construction SCript
 	virtual void OnConstruction(const FTransform& Transform) override;
 
+	UFUNCTION()
+	int32 GetRandomInt(int32 Min, int32 Max)
+	{
+		FMath::RandInit(FDateTime::UtcNow().GetTicks() + GetUniqueID());
+		return FMath::RandRange(Min, Max);
+	}
+
 
 public:	
+
+	UPROPERTY(EditAnywhere)
+	TArray<ABaseTrafficVehicle*> TrafficLibrary;
+	UPROPERTY(EditAnywhere)
+	FVector PlayerWorldLocation;
+	UPROPERTY(EditAnywhere)
+	float MaxVehicleDistance = 10000;
+	UPROPERTY(EditAnywhere)
+	float MaxLaneSearchDistance = 2000;
+	UPROPERTY(EditAnywhere)
+	int TrafficAgentsPoolSize = 15;
+	UPROPERTY(EditAnywhere)
+	TArray<FTrafficAgents> TrafficAgentsPool;
+	UPROPERTY(EditAnywhere)
+	TArray<FLanes> AvailableRoads;
+	UPROPERTY(EditAnywhere)
+	TArray<int> TrafficAgentRespawnList;
+
+	UPROPERTY(EditAnywhere)
+	TArray<ALaneSpline*> CurrentSpawnableLanes;
+	
+	//Grid Properties
+	UPROPERTY(EditAnywhere)
+	bool RebuildGrid = false;
+	UPROPERTY(EditAnywhere)
+	FVector GridWorldBounds = FVector(5000,5000,5000);
+	UPROPERTY(EditAnywhere)
+	FVector GridResolution = FVector(10,10,10);
+	UPROPERTY(EditAnywhere)
+	int GridDivisions = 10.0f;
+	UPROPERTY(VisibleAnywhere)
+	FVector IndividualCellSize;
+	UPROPERTY(VisibleAnywhere)
+	FVector CellsOffsetSpace;
+
+
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<FLaneSplinePoints> RoadPoints;
+	UPROPERTY(VisibleAnywhere)
+	TArray<ATrafficGridObject*> TrafficCells;
+	UPROPERTY(VisibleAnywhere)
+	TArray<FVector> CellPositions;
+	UPROPERTY()
+	int ActiveCellIndex = 0;
+	UPROPERTY(VisibleAnywhere)
+	TArray<ATrafficGridObject*>ActiveSpawnCells;
+
+	//Spawn Collision Data
+	FCollisionQueryParams CollisionParms;
+
+
+	float ElapsedTimer;
+
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
